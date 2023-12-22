ba9d0e5c4b9c8254e4855e675b67de831efda4c5
Swapnil Opash <sra.opash@gmail.com>
2023-12-22 11:32:01 +0530

    change bucket

--- src/helpers/aws-s3.helper.js
+++ src/helpers/aws-s3.helper.js
@@ -5,7 +5,7 @@ const s3 = new AWS.S3({
   accessKeyId: "XZ1L2U32Z7XMOW5S5ZBD",
   secretAccessKey: "2e3lYJoXmocA5W3mVSpaDQF4qrDbbUA3kuFOO2Pe",
   endpoint: new AWS.Endpoint("s3.wasabisys.com"), // Wasabi endpoint
-  region: "us-east-2",
+  region: "us-east-1",
 });
 exports.uploadFileToWasabi = async (file, key) => {
   return new Promise((resolve, reject) => {
