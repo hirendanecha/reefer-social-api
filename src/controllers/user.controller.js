56100ffbf6b71152cef7aa17f3d6ea807426e3ab
htk-opash <htk.opash@gmail.com>
2024-03-05 18:24:21 +0530

    Secured socekts, routes & sensitive data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ const User = require("../models/user.model");
 const utils = require("../helpers/utils");
 const environments = require("../environments/environment");
 const jwt = require("jsonwebtoken");
+const authorize = require("../middleware/authorize");
 
 const { getPagination, getCount, getPaginationData } = require("../helpers/fn");
 const { Encrypt } = require("../helpers/cryptography");
@@ -340,9 +341,13 @@ exports.delete = function (req, res) {
   const userId = req.params.id;
   const profileId = req.query.profileId;
   console.log(userId, profileId);
-  const isDeleted = User.delete(userId, profileId);
-  if (isDeleted) {
-    res.json({ error: false, message: "User deleted successfully" });
+  if (req.query.profileId === req.user.id) {
+    const isDeleted = User.delete(userId, profileId);
+    if (isDeleted) {
+      res.json({ error: false, message: "User deleted successfully" });
+    }
+  } else {
+    return res.status(401).json({ message: "Unauthorized token" });
   }
 };
 
@@ -470,6 +475,8 @@ exports.resendVerification = function (req, res) {
 
 exports.logout = function (req, res) {
   console.log("cookies");
+  const token = req.headers.authorization.split(" ")[1];
+  authorize.setTokenInList(token);
   res.clearCookie("auth-user", {
     sameSite: "none",
     secure: true,
@@ -481,7 +488,7 @@ exports.logout = function (req, res) {
   //   sameSite: "none",
   //   domain: environments.domain,
   // });
-  res.end();
+  return res.status(200).json({ message: "logout successfully" });
 };
 
 exports.getStats = async function (req, res) {
@@ -494,5 +501,21 @@ exports.getStats = async function (req, res) {
     } else {
       res.status(404).send({ message: "not found" });
     }
+  }
+};
+
+exports.verifyToken = async function (req, res) {
+  try {
+    const token = req.params.token;
+    const decoded = jwt.verify(token, environments.JWT_SECRET_KEY);
+    if (decoded.user) {
+      res.status(200).send({ message: "Authorized User", verifiedToken: true });
+    } else {
+      res
+        .status(401)
+        .json({ message: "Unauthorized Token", verifiedToken: false });
+    }
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token", verifiedToken: false });
   }
 };
