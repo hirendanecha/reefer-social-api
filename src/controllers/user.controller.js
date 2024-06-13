a134064487132bbddeaf4d43fc89eef13779591b
Swapnil Opash <sra.opash@gmail.com>
2024-06-13 19:54:57 +0530

    change route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -447,7 +447,7 @@ exports.verification = function (req, res) {
     const token = await generateJwtToken(data);
     console.log(token);
     return res.redirect(
-      `${environments.FRONTEND_URL}/healing-registration?token=${token}`
+      `${environments.FRONTEND_URL}/reefer-registration?token=${token}`
     );
   });
 };
